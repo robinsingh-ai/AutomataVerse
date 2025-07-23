c915607e88c6a4d55d038e8ae294221b449421b1
Ruhani120 <3910ruhanigupta@gmail.com>
2025-07-23 21:51:25 +0530

    something forget to save

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -17,10 +18,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-//const auth = getAuth(app);
+const auth = getAuth(app);
 const getCurrentUser = () => {
-  return null
+  return getAuth(app).currentUser;
 };
 
 
-export { app, auth, db, getCurrentUser };
+export { app, db, auth, getCurrentUser }; 
